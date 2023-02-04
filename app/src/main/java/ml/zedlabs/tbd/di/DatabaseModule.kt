68924ef3853ad1cf8fae98da927073a75ccd48bc
zohaib <zohaibansari100@gmail.com>
2023-02-04 21:03:48 +0530

    add transaction DO components

---
+++ app/src/main/java/ml/zedlabs/tbd/di/DatabaseModule.kt
@@ -0,0 +1,59 @@
+package ml.zedlabs.tbd.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import ml.zedlabs.tbd.databases.expense_type_db.ExpenseTypeDao
+import ml.zedlabs.tbd.databases.expense_type_db.ExpenseTypeDatabase
+import ml.zedlabs.tbd.databases.transaction_db.TransactionDao
+import ml.zedlabs.tbd.databases.transaction_db.TransactionDatabase
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideTransactionDb(
+        @ApplicationContext applicationContext: Context
+    ): TransactionDatabase {
+        return Room
+            .databaseBuilder(
+                applicationContext,
+                TransactionDatabase::class.java, "transaction-list-db"
+            ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTransactionDao(transactionDb: TransactionDatabase): TransactionDao {
+        return transactionDb.transactionDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTypeDb(
+        @ApplicationContext applicationContext: Context
+    ): ExpenseTypeDatabase {
+        return Room
+            .databaseBuilder(
+                applicationContext,
+                ExpenseTypeDatabase::class.java, "expense-type-db"
+            ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideTypeDao(expenseTypeDatabase: ExpenseTypeDatabase): ExpenseTypeDao {
+        return expenseTypeDatabase.expenseTypeDao()
+    }
+}
