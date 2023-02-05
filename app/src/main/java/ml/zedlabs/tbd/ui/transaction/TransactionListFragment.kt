5b7c19fc2ce8ee9d63f86fbb23f6c7335e7031c0
zohaib <zohaibansari100@gmail.com>
2023-02-05 12:54:51 +0530

    add bottom sheet initial hadnling

--- app/src/main/java/ml/zedlabs/tbd/ui/transaction/TransactionListFragment.kt
+++ app/src/main/java/ml/zedlabs/tbd/ui/transaction/TransactionListFragment.kt
@@ -6,23 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetState
+import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.paging.compose.items
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import ml.zedlabs.tbd.MainViewModel
 import ml.zedlabs.tbd.ui.common.LargeText
 import ml.zedlabs.tbd.ui.common.MediumText
@@ -54,33 +64,40 @@ class TransactionListFragment : Fragment() {
     }
 
     @Composable
-    fun TransactionListParent() {
-        BottomSheetScaffold(sheetContent = {
-            AddTransactionBottomSheet()
-        }) {
+    fun TransactionListParent(mod: Modifier = Modifier) {
+        val addTransactionSheetState =
+            rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+        val scaffoldState =
+            rememberBottomSheetScaffoldState(bottomSheetState = addTransactionSheetState)
+        val scope = rememberCoroutineScope()
+
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 0.dp,
+            sheetContent = {
+                AddTransactionBottomSheet()
+            },
+            modifier = mod.pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    scope.launch {
+                        addTransactionSheetState.collapse()
+                    }
+                })
+            }
+        ) {
             val listItems =
                 transactionViewModel.getUsersTransactions().collectAsState(initial = listOf()).value
 
             LazyColumn(
-                modifier = Modifier
+                modifier = mod
                     .background(color = MaterialTheme.colors.secondary)
                     .fillMaxSize()
                     .padding(horizontal = 12.dp)
             ) {
                 item {
-                    Spacer24()
-                    LargeText(
-                        text = "Welcome to transaction list",
-                        color = MaterialTheme.colors.onSecondary
-                    )
-                    Spacer24()
-                    MediumText(
-                        text = "Add Expense / Income",
-                        modifier = Modifier.clickable {
-                            // open the bottom sheet
-                            transactionViewModel.addFakeEntryToTransactionDb()
-                        },
-                        color = MaterialTheme.colors.onSecondary
+                    TransactionHeaderItem(
+                        scope = scope,
+                        addTransactionSheetState = addTransactionSheetState
                     )
                 }
                 items(items = listItems) { item ->
@@ -91,8 +108,43 @@ class TransactionListFragment : Fragment() {
     }
 
     @Composable
-    private fun AddTransactionBottomSheet() {
+    fun TransactionHeaderItem(
+        mod: Modifier = Modifier,
+        scope: CoroutineScope,
+        addTransactionSheetState: BottomSheetState
+    ) {
+        Spacer24()
+        LargeText(
+            text = "Welcome to transaction list",
+            color = MaterialTheme.colors.onSecondary
+        )
+        Spacer24()
+        MediumText(
+            text = "Add Expense / Income",
+            modifier = mod.clickable {
+                // open the bottom sheet
+                scope.launch {
+                    addTransactionSheetState.expand()
+                }
+            },
+            color = MaterialTheme.colors.onSecondary
+        )
+    }
 
+    @Composable
+    private fun AddTransactionBottomSheet(mod: Modifier = Modifier) {
+        Box(modifier = mod
+            .fillMaxWidth()
+            .clickable { }) {
+            MediumText(text = "Add Fake",
+                color = MaterialTheme.colors.onSecondary,
+                modifier = mod
+                    .clickable {
+                        transactionViewModel.addFakeEntryToTransactionDb()
+                    }
+                    .padding(20.dp)
+            )
+        }
     }
 
 
